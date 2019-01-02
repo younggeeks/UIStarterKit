4def8e049b390e18fa27ca8b1a7084886bed0aef
Samwel Charles <younggeeks101@gmail.com>
2019-01-02 08:44:40 +0300

    add gradient view class

---
+++ Classes/GradientView.swift
@@ -0,0 +1,64 @@
+//
+//  GradientView.swift
+//  FBSnapshotTestCase
+//
+//  Created by Samwel Charles on 02/01/2019.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    @IBInspectable var firstColor : UIColor = UIColor.clear {
+        didSet{
+            updateView()
+        }
+    }
+    
+    @IBInspectable var secondColor : UIColor = UIColor.clear {
+        didSet{
+            updateView()
+        }
+    }
+    
+    
+    @IBInspectable var locations : [Int] = [] {
+        didSet{
+            updateView()
+        }
+    }
+    
+    
+    
+    @IBInspectable var startPoint:CGPoint = CGPoint(x: 0, y: 0){
+        didSet{
+            updateView()
+        }
+    }
+    
+    @IBInspectable var endPoint:CGPoint = CGPoint(x: 0, y: 1){
+        didSet{
+            updateView()
+        }
+    }
+    
+    
+    override class var layerClass : AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView(){
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map{$0.cgColor}
+        layer.startPoint = startPoint
+        layer.endPoint = endPoint
+        
+        //TODO : add support for locations
+    }
+    
+}
